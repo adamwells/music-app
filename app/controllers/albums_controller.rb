1598620fb51804be884e5ff508032d9a6253617f
Adam Wells <adamwells.dev@gmail.com>
2015-03-12 15:27:53 -0700

    Add basic Band functionality

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,30 @@
+class AlbumsController < ApplicationController
+  def new
+    @bands = Band.all
+    @band = Band.find(params[:band_id])
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      flash[:notice] = "You've successfully created #{ @album.name }"
+      redirect_to @album
+    else
+      @bands = Band.all
+      @band = Band.find(@album.band_id)
+      flash.now[:errors] = @album.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:name, :band_id)
+  end
+end
---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,26 @@
+class BandsController < ApplicationController
+  def new
+    @band = Band.new
+  end
+
+  def create
+    @band = Band.new(band_params)
+    if @band.save
+      flash[:notice] = "Congratulations on creating #{@band.name}'s page!"
+      redirect_to @band
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @band = Band.find(params[:id])
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
+  end
+end
